a89cd7a62a8880d7528d5c31a6896d0a208c4689
DESKTOP-EF2VHJM\ivana <dobrevolje1@gmail.com>
2023-07-02 12:55:10 +0200

    slider comp

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { SliderData } from "./SliderData"
+import{ FaArrowCircleLeft, FaArrowCircleRight} from "react-icons/fa"
 
 
 
@@ -13,12 +15,45 @@ interface MyComponentProps {
 
 
 const Slider: React.FC<MyComponentProps> = ({ images }) => {
+
+  const [current, setCurrent] = useState(0)
+  const length = images.length
+
+  const nextSlide= () => {
+    setCurrent(current===length - 1 ? 0 : current + 1  )
+  }
+
+  const prevSlide= () => {
+    setCurrent(current===0 ? length -1 : current - 1  )
+  }
+
+ if(!Array.isArray(images)  || images.length<=0) {
+  return null
+ }
+
   return (
-    <div >
-      {images.map((image, index) => (
-        <img className="w-[900px] h-[600px] object-cover" key={index} src={image.src} alt={image.alt} />
-      ))}
-    </div>
+    <div id="gallery" className="m-8">
+      <h1 className="text-3xl font-bold text-center p-8">Galerija</h1>
+      <div className="relative flex justify-center items-center bg-white">
+       {images.map((image, index) => {
+        return (
+          <div className={index===current? "opacity-[1] ease-in duration-1000 object-cover": "opacity-0"}>
+       <FaArrowCircleLeft onClick={prevSlide} className="absolute top-[50%] left-[180px] text-black/70 cursor-pointer select-none z-[2] " size={40}></FaArrowCircleLeft>
+            {index===current && (
+         
+       <img  className="w-[800px] h-[500px] "key={index} src={image.src} alt={image.alt} />
+             
+            )}
+                 <FaArrowCircleRight onClick={nextSlide} className="absolute top-[50%] right-[180px] text-black/70 cursor-pointer select-none z-[2] " size={40}></FaArrowCircleRight>
+                 </div>
+         )}
+     
+        )
+       
+       
+            }
+      </div>
+     </div>
   );
 };
 
